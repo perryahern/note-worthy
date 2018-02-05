74731bced05bd183563ca84fecc0532f6678eabb
perryahern <pahern@gmail.com>
2018-02-05 12:46:25 -0500

    Basic React app setup completed

---
+++ note-worthy-app/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Note-worthy</h1>
+          <h3 className="App-subtitle">If it's worthy of a note, it's Note-worthy</h3>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
